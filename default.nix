45a0b27a934d49b67b8cfd5b178f8fac1aa6e65a
Jonathan Boyle <programmerfalcon@gmail.com>
2024-11-09 11:30:10 +0000

    feat: remove alvr

--- default.nix
+++ default.nix
@@ -22,7 +22,6 @@ in
 	# example-package = pkgs.callPackage ./pkgs/example-package { };
 	# some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
 	# ...
-	alvr = pkgs.callPackage pkgs/alvr {};
 	g13d = pkgs.callPackage pkgs/g13d {};
 	gitignore-template = p_311.callPackage pkgs/gitignore-template {};
 	python-jwt_310 = p_310.callPackage pkgs/python-jwt {python-ver = 310;};
